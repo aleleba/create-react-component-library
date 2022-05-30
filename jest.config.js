c693b4f6a2f7aaa0756f53a6f47f3ebcefe43746
Alejandro Lembke Barrientos <aleleba@hotmail.com>
2022-05-30 17:25:45 +0000

    PR-188649: Adding Test to proyect.

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+
+   
+module.exports = {
+    setupFilesAfterEnv: ['<rootDir>/setupTest.ts'],
+    "testEnvironment": "jsdom",
+    moduleNameMapper: {
+      "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/src/__mocks__/fileMock.ts",
+      "\\.(css|sass|less)$": "identity-obj-proxy"
+    },
+  };
