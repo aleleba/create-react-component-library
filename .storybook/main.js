e9d0291916a414457122ce9b6f88efef333341fd
Alejandro Lembke Barrientos <aleleba@hotmail.com>
2023-03-28 23:36:47 +0000

    PR-912037: updating packages.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,39 +1,33 @@
 const path = require('path');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
-
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-postcss",
-    "@storybook/preset-scss"
-  ],
+  "stories": ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-postcss", "@storybook/preset-scss", "@storybook/addon-mdx-gfm"],
   "webpackFinal": async config => {
-    config.resolve.alias = { ...config.resolve.alias,
+    config.resolve.alias = {
+      ...config.resolve.alias,
       '@components': path.resolve(__dirname, "../src/components/")
     };
     config.resolve.plugins = [new TsconfigPathsPlugin()];
     return config;
   },
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
+  "framework": {
+    name: "@storybook/react-webpack5",
+    options: {}
   },
   typescript: {
     reactDocgenTypescriptOptions: {
       compilerOptions: {
         "paths": {
-          "@Components/*": ["Components/*"],
-        },
+          "@Components/*": ["Components/*"]
+        }
       }
     }
   },
   features: {
-    previewMdx2: true,
+    previewMdx2: true
   },
-}
+  docs: {
+    autodocs: true
+  }
+};
