6a432591375a1d090e19cfc363faf2878a5d72c9
Alejandro Lembke Barrientos <aleleba@hotmail.com>
2025-07-16 14:37:53 +0000

    PR-342468: fixing test.

--- src/components/__tests__/Card.test.tsx
+++ src/components/__tests__/Card.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { Card } from '@components';
 
-describe('<App/> Component', () => {
+describe('<Card/> Component', () => {
     beforeEach(() => {
         // fetchMock.resetMocks();
         render(<Card title='Test Title'><p>Test Content</p></Card>)
@@ -12,13 +12,13 @@ describe('<App/> Component', () => {
             //First Data Fetch
             data: 'data'
         })); */
-        screen.getByText('Test Title')
+        expect(screen.getByText('Test Title')).toBeInTheDocument();
     })
     it('Show Child Component', async () => {
         /* fetchMock.mockResponseOnce(JSON.stringify({
             //First Data Fetch
             data: 'data'
         })); */
-        screen.getByText('Test Content')
+        expect(screen.getByText('Test Content')).toBeInTheDocument();
     })
 })
