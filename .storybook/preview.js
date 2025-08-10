9210349485b4e61c2fdbffe5e4bbab6ae957db9d
Alejandro Lembke Barrientos <aleleba@hotmail.com>
2025-08-10 05:22:04 +0000

    PR-500824updating packages.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -6,5 +6,24 @@ export const parameters = {
       date: /Date$/,
     },
   },
+  docs: {
+    extractArgTypes: (component) => {
+      // Filter out Symbol values to prevent serialization errors
+      const argTypes = {};
+      if (component && component.propTypes) {
+        Object.keys(component.propTypes).forEach(key => {
+          const propType = component.propTypes[key];
+          // Only include non-Symbol values
+          if (typeof propType !== 'symbol') {
+            argTypes[key] = propType;
+          }
+        });
+      }
+      return argTypes;
+    },
+    source: {
+      excludeDecorators: true,
+    },
+  },
 }
 export const tags = ["autodocs"];
--- src/components/Card/Card.stories.tsx
+++ src/components/Card/Card.stories.tsx
@@ -1,19 +1,37 @@
 import React from 'react';
-import { StoryFn, Meta } from '@storybook/react-webpack5';
+import type { Meta, StoryObj } from '@storybook/react';
 import { Card } from '@components';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
-export default {
+const meta: Meta<typeof Card> = {
   title: 'Example/Card',
   component: Card,
-} as Meta<typeof Card>;
+  parameters: {
+    docs: {
+      description: {
+        component: 'A reusable Card component for displaying content with an optional title.',
+      },
+    },
+  },
+  argTypes: {
+    title: {
+      control: 'text',
+      description: 'The title of the card',
+    },
+    children: {
+      control: false,
+      description: 'The content to display inside the card',
+    },
+  },
+};
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: StoryFn<typeof Card> = (args) => <Card {...args} />;
+export default meta;
+type Story = StoryObj<typeof meta>;
 
-export const Basic = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
-Basic.args = {
-  title: 'Test Title',
-  children: <p>Test Content</p>,
+export const Basic: Story = {
+  args: {
+    title: 'Test Title',
+    children: <p>Test Content</p>,
+  },
 };
